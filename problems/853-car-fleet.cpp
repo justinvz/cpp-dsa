e53c44fd91915d8633c49d124cc18f5f98cea18b
justinvz <justin.vz@live.nl>
2026-07-26 20:18:39 +0200

    Revisted car fleet

--- problems/853-car-fleet.cpp
+++ problems/853-car-fleet.cpp
@@ -5,51 +5,47 @@
 
 using namespace std;
 
-/// @problem
-/// There are n cars at given miles away from the startng mile 0, travelling to
-/// reach the mile target.
-///
-/// you are given two integer arrays 'position' and 'speed' both of length 'n',
-/// where 'position[i]' is the starting mile of the 'i th' mile per hour.
-///
-/// A car cannot pass another car, but it can catch up and then travel nexct to
-/// it at the pseed of the slower car.
-///
-/// a car fleet is a single car or a group of cars driving next to each other.
-/// The speed of the car fleet is the minimum speed of any car in the fleet.
-///
-/// if a car catches up to a car fleet at the mile 'target', it will still be
-/// considered as part of the car fleet.
-///
-/// return the number of car fleets that will arrive at the destination.
+/// @ revisit 3
+/// @ startTime = 26-07-2026 19:42
+/// @ finish = 26-07-2026 20:10
 
 struct Car {
-  int pos;
-  int speed;
+  int startPos{0};
+  float TimeOfArrival{0};
 };
 
 int CarFleet(int target, vector<int> position, vector<int> speed) {
+
+  // Sort time of arrivles based on accending starting position.
+
   std::vector<Car> cars;
 
-  for (size_t i{0}; i < position.size(); i++) {
-    cars.emplace_back(Car{position[i], speed[i]});
-  }
+  for (size_t i = 0; i < position.size(); i++) {
+    cars.emplace_back(
+        Car{.startPos = position[i],
+            .TimeOfArrival = (target - position[i]) / (float)speed[i]});
+  };
 
-  std::sort(cars.begin(), cars.end(),
-            [](const Car &a, const Car &b) { return a.pos < b.pos; });
+  std::ranges::sort(cars,
+                    [](Car &a, Car &b) { return a.startPos > b.startPos; });
 
-  std::stack<int> fleets;
-  for (int i = cars.size() - 1; i >= 0; --i) {
-    double time = static_cast<double>(target - cars[i].pos) / cars[i].speed;
+  std::stack<float> fleet;
 
-    std::cout << time << std::endl;
-
-    if (fleets.empty() || time > fleets.top()) {
-      fleets.push(time);
+  for (auto car : cars) {
+    if (fleet.empty()) {
+      fleet.push(car.TimeOfArrival);
+      continue;
     }
+
+    if (car.TimeOfArrival > fleet.top()) {
+      fleet.push(car.TimeOfArrival);
+    }
+
+    println("cars pos {} and timoe of arrival {}", car.startPos,
+            car.TimeOfArrival);
   }
 
-  return fleets.size();
+  return fleet.size();
 }
 
 int main() {
@@ -58,5 +54,5 @@ int main() {
   ExpectEq(CarFleet(12, {10, 8, 0, 5, 4}, {2, 4, 1, 1, 3}), 3);
   ExpectEq(CarFleet(10, {3}, {3}), 1);
   ExpectEq(CarFleet(100, {0, 2, 4}, {4, 2, 1}), 1);
-  ExpectEq(CarFleet(10, {0, 4, 2}, {2, 1, 3}), 1);
+  ExpectEq(CarFleet(10, {0, 4, 2}, {2, 1, 3}), 2);
 }
